a82ea939344eae126caab12ab5010804b8f7083d
Oliver Bähler <oliverbaehler@hotmail.com>
2021-01-11 22:02:18 +0100

    Initial Publish

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# helm-manifests-plugin
+# Helm Manifests Plugin
+
+The Plugin is used to currently render values for the [Helm Manifests Chart](https://github.com/bedag/helm-charts/tree/master/charts/manifests). It's a simple bash wrapper but does not require you to download the entire github repository. More features might be added in the future.
+
+# Install
+
+You can directly install the plugin by referencing this repository:
+
+```
+helm plugin install https://github.com/bedag/helm-manifests-plugin.git
+```
+
+# Uninstall
+
+To remove the plugin, use the following command:
+
+```
+helm plugin uninstall manifests
+```
+
+# Usage
+
+Learn which arguments are available:
+
+```
+helm manifests -h
+```
+
+# Examples
+
+NOTE: You can only get the values for one manifest **or** preset per command.
+
+Just running the plugin without any parameters return common values/extra values which don't apply to a single resource.
+
+```
+helm manifests
+```
+
+Render Values for a `Statefulset` Manifest:
+
+```
+helm manifests -m "statefulset"
+```
+
+With Minimal Output:
+
+```
+helm manifests -m "statefulset" -M
+```
+
+Get all values for a service under the parentkey `headless`:
+
+```
+helm manifests -m "service" -k "headless"
+```
+
+Get the `jmxExporter` preset under the custom path `jmxExporter.*`:
+
+```
+helm manifests -p "jmxexporter" -P "jmxExporter"
+```
+
+# Contributing
+
+We'd love to have you contribute! Please refer to our [contribution guidelines](CONTRIBUTING.md) for details.
+
+**By making a contribution to this project, you agree to and comply with the
+[Developer's Certificate of Origin](./DCO).**
+
+# License
+
+[Apache 2.0 License](./LICENSE).
---
+++ generator.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Copyright © 2021 Oliver Baehler
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+## -- Help Context
+show_help() {
+cat << EOF
+
+Usage: ${0##*/} [-h] [ [-m] manifest || [-p] preset ] [-k] parentkey [-P] keypath [-a] helm_args [-M]
+    -m [manifest]      Manifest Name
+    -p [preset]        Preset Name
+    -k [parentkey]     Custom Parent Key
+    -P [keypath]       Custom Key Path
+    -a [helm args]     Additional Helm Arguments
+    -M                 Minimal Structure
+    -h                 Show this context
+
+EOF
+}
+
+## -- Defaults
+MANIFEST=""
+PRESET=""
+HELM_ARGS=""
+KEYPATH=""
+MINIMAL=""
+PARENTKEY=""
+
+## -- Opting arguments
+OPTIND=1; # Reset OPTIND, to clear getopts when used in a prior script
+while getopts ":ha:P:k:m:p:k:M" opt; do
+  case ${opt} in
+    m)
+       MANIFEST="${OPTARG}"
+       ;;
+    p)
+       PRESET="${OPTARG}"
+       ;;
+    k)
+       PARENTKEY="${OPTARG}"
+       ;;
+    P)
+       KEYPATH="${OPTARG}"
+       ;;
+    a)
+       HELM_ARGS="${OPTARG}"
+       ;;
+    M)
+       MINIMAL="true"
+       ;;
+    h)
+       show_help
+       exit 0
+       ;;
+    ?)
+       echo "Invalid Option: -$OPTARG" 1>&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND -1))
+
+## -- Helm Values
+HELM_VALUES=""
+[ -n "$MANIFEST" ] && HELM_VALUES="--set doc.manifest=${MANIFEST}"
+[ -n "$PRESET" ] && HELM_VALUES="${HELM_VALUES} --set doc.preset=${PRESET}"
+[ -n "$KEYPATH" ] && HELM_VALUES="${HELM_VALUES} --set doc.path=${KEYPATH}"
+[ -n "$PARENTKEY" ] && HELM_VALUES="${HELM_VALUES} --set doc.key=${PARENTKEY}"
+[ -n "$MINIMAL" ] && HELM_VALUES="${HELM_VALUES} --set doc.minimal=true"
+
+## -- Execute Helm
+$HELM_BIN template $HELM_VALUES ${HELM_ARGS} --dependency-update $HELM_PLUGIN_DIR/values-generator/
